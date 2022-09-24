5bb0a90df85aac7f5edd29fd0a9d5226dff1c9ee
Rosana Sato <rosana.sato@protonmail.com>
2022-09-24 20:44:53 -0300

    envia arquivos de configuração inicial

---
+++ src/test/java/com/fastforward/cardapi/CardApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fastforward.cardapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CardApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
