913f8c09366431e122b8ddbc378289972eae5836
Rosana Sato <rosana.sato@protonmail.com>
2022-09-26 21:03:28 -0300

    cria endpoint health em CardController

--- src/main/java/com/fastforward/cardapi/controller/CardController.java
+++ src/main/java/com/fastforward/cardapi/controller/CardController.java
@@ -1,2 +1,24 @@
-package com.fastforward.cardapi.controller;public class CardController {
+package com.fastforward.cardapi.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/card")
+public class CardController{
+    private static final Logger LOG = LoggerFactory.getLogger(CardController.class);
+
+    //GET localhost:8080/card/health
+    @ResponseStatus(HttpStatus.OK)
+    @GetMapping(value = "/health", produces = "application/json")
+    public String health(){
+        LOG.info("Avaliando saúde da aplicação");
+        return "{\"status\":\"ok\"}";
+    }
+
 }
